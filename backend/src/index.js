988501e53c5d5fd1e61c597460d47b8069d4f280
Astitva Srivastava <srivastaway@gmail.com>
2026-07-03 10:44:53 +0530

    added / for making dist static

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes);
 
 if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join(__dirname,"..frontend/dist")))
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
     app.get("/{*splat}",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
